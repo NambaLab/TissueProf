ac924b547ad6c8a54d9e1cbd9cc8e586db3c787d
Emre Dusunceli <dusuncelie@gmail.com>
2025-03-11 07:54:04 +0200

    Bump to 1.0.0

--- TissueProf/src/main/java/fi/helsinki/TissueProf.java
+++ TissueProf/src/main/java/fi/helsinki/TissueProf.java
@@ -1,6 +1,13 @@
-//TissueProf v1.0.0
-//Emre Dusunceli, Namba Group, University of Helsinki
+/*
+TissueProf v1.0.0
+Emre Dusunceli, Namba Group, University of Helsinki, 2025
+This plugin is used to analyze the co-expression of multiple genes/proteins/molecules of interest in cells 
+that are found in one or more zones of the target tissue. It takes as input a multi-channel confocal image
+ and outputs a Results file in excel format as well as various ROI sets and images that are saved during
+  analysis for tracing the data and later review.
 
+TissueProf has been developed by Emre Dusunceli in the Namba Lab (ORCID ID: 0000-0003-4875-666X).
+*/
 package fi.helsinki;
 
 import java.awt.Button;
